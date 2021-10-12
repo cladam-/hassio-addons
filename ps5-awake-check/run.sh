07e4294f537a161b079eaf80fd024182d58ac3ef
Carl-Adam <cladam@gmail.com>
2021-10-12 17:28:14 +0200

    less mqtt spam

--- ps5-awake-check/run.sh
+++ ps5-awake-check/run.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/with-contenv bashio
+
+username=$(bashio::config "mqttuser")
+password=$(bashio::config "mqttpass")
+mqtthost=$(bashio::config "mqtthost")
+mqttport=$(bashio::config "mqttport")
+
+mosquitto_pub -h "$mqtthost" -p "$mqttport" -u "$username" -P "$password" -t homeassistant/binary_sensor/ps5mqtt/config -m "{\"name\":\"ps5mqtt\",\"state_topic\":\"homeassistant/binary_sensor/ps5mqtt/state\"}"
+echo 'Running!'
 while true ; do ./script & sleep 5; done
