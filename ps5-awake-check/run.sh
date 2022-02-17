04a198c6b5e0e42de6d2d12ba46142d1aeccb8b7
Carl-Adam Biesiada <cladam@gmail.com>
2022-02-18 00:28:23 +0100

    start off

--- ps5-awake-check/run.sh
+++ ps5-awake-check/run.sh
@@ -6,5 +6,6 @@ mqtthost=$(bashio::config "mqtthost")
 mqttport=$(bashio::config "mqttport")
 
 mosquitto_pub -h "$mqtthost" -p "$mqttport" -u "$username" -P "$password" -t homeassistant/binary_sensor/ps5mqtt/config -m "{\"name\":\"PlayStation 5\",\"state_topic\":\"homeassistant/binary_sensor/ps5mqtt/state\"}"
+mosquitto_pub -h "$mqtthost" -p "$mqttport" -u "$username" -P "$password" -t homeassistant/binary_sensor/ps5mqtt/state -m "OFF"
 echo 'Running!'
 while true ; do ./script & sleep 5; done
